9164731fb3680eab9b3d6fdff6607fa80e917c64
John Resig <jeresig@gmail.com>
2013-12-18 23:22:45 -0500

    Add in manifest generation, stream piping, express support.

--- appcache.js
+++ appcache.js
@@ -1,11 +1,12 @@
 var async = require("async");
 var glob = require("glob");
 
-var AppCache = function() {
+var AppCache = function(options) {
+    this._options = options || {};
+
     this._data = {
         cache: [],
-        network: [],
-        fallback: []
+        network: []
     };
 
     this._globbedFiles = {};
@@ -14,71 +15,149 @@ var AppCache = function() {
 };
 
 AppCache.prototype = {
-    cache: function(files) {
+    addCache: function(files) {
         if (typeof files === "string") {
             files = [files];
         }
 
         this._data.cache = this._data.cache.concat(files);
         this._dirty = true;
+
+        return this;
     },
 
-    network: function(files) {
+    addNetwork: function(files) {
         if (typeof files === "string") {
             files = [files];
         }
 
         this._data.network = this._data.network.concat(files);
         this._dirty = true;
+
+        return this;
     },
 
-    glob: function(file, callback) {
+    reload: function() {
+        this._dirty = true;
+        this.buildCache();
+        return this;
+    },
+
+    getString: function(callback) {
+        var self = this;
+
+        this._buildCache(function(err) {
+            callback(err, self._buildString());
+        });
+    },
+
+    pipe: function(dest) {
+        this.getString(function(err, data) {
+            // Better handle piping directly to an HTTP response
+            if (dest.setHeader) {
+                // Set the correct header for the file
+                dest.setHeader("Content-Type", "text/cache-manifest");
+
+                // Set it to expire immediately, to make sure it's never cached
+                dest.setHeader("Expires", "0");
+            }
+
+            dest.emit(data);
+        });
+
+        return dest;
+    },
+
+    clone: function() {
+        var clone = new AppCache();
+
+        clone._data = {
+            cache: this.data.cache.slice(0),
+            network: this.data.network.slice(0)
+        };
+
+        return clone;
+    },
+
+    route: function() {
+        var self = this;
+
+        return function(req, res) {
+            self.pipe(res);
+        };
+    },
+
+    _buildString: function() {
+        var str = [];
+
+        var addFiles = function(name) {
+            this._data[name].forEach(function(file) {
+                if (typeof file === "string") {
+                    str.push(file);
+                } else {
+                    str = str.concat(file);
+                }
+            })
+        };
+
+        str.push("# " + this._key);
+
+        if (this._data.cache.length) {
+            str.push("CACHE:");
+            addFiles("cache");
+        }
+
+        if (this._data.network.length) {
+            str.push("NETWORK:");
+            addFiles("network");
+        }
+
+        return str.join("\n");
+    },
+
+    _glob: function(file, callback) {
         if (file.indexOf("http") === 0) {
             callback(null, file);
         } else {
-            glob(file, callback);
+            var options = {};
+
+            if (this._options.cwd) {
+                options.cwd = this._options.cwd;
+            }
+
+            glob(file, options, callback);
         }
     },
 
-    getFiles: function(files, callback) {
-        async.concat(files, this.glob, callback);
+    _bump: function() {
+        this._key = (new Date).getTime() + Math.random();
     },
 
-    bump: function() {
-        return (this._key = (new Date).getTime() + Math.random());
-    },
-
-    buildCache: function(callback) {
+    _buildCache: function(callback) {
         var self = this;
 
         if (!this._dirty) {
-            return callback();
+            return process.nextTick(callback);
         }
 
-        this.bump();
+        this._dirty = false;
+        this._globbedFiles = {};
+
+        this._bump();
 
         async.each(Object.keys(this._data), function(key, callback) {
-            self.getFiles(self._data[key], function(err, files) {
+            async.concat(self._data[key], self._glob, function(err, files) {
                 self._globbedFiles[key] = files;
                 callback(err);
             });
         }, callback);
 
-        this._dirty = false;
-    },
-
-    clone: function() {
-        
-
+        return this;
     }
 };
 
-// Inheritance
-// Middleware for Express
 module.exports = {
-    create: function() {
-
+    create: function(options) {
+        return new AppCache(options);
     }
-
-
 };
